f740f36c7cde3760a54b2d32a080bf7a155e9318
ArianneMS <164070707+ArianneMS@users.noreply.github.com>
2024-04-07 16:19:18 +0000

    Added columns

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,16 @@ import Layout from './components/Layout';
 
 export default function App() {
   return( 
-   <Layout>
-   Schimb textul
-   </Layout>
+   <Layout 
+     left ={
+      <div>left side</div>
+     }
+     right ={
+      <div>right side</div>
+     }
+     
+     />
+   
   
     )
 }
